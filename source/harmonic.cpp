03857dc464dcd2a56ca8f459e3fb720d7e4324f3
Tomoaki <87553219+Tomoamura@users.noreply.github.com>
2025-01-21 16:23:23 +0900

    Langevin

--- source/harmonic.cpp
+++ source/harmonic.cpp
@@ -1,4 +1,4 @@
-// g++ -std=c++14 -I/opt/homebrew/opt/boost/include harmonic.cpp -o harmonic
+// g++ -std=c++14 -O2 -I/opt/homebrew/opt/boost/include harmonic.cpp -o harmonic
 
 #include <boost/numeric/odeint.hpp>
 #include <vector>
@@ -27,7 +27,7 @@ void output_to_file(state_type &x, double t, std::ofstream &outfile) {
 }
 
 void AddNoise(state_type &x, double t) {
-  x[0] += 0.0003*dist(engine);
+  x[0] += 0.005*dist(engine);
 }
 
 
@@ -76,7 +76,7 @@ int main() {
 			double prec = 1.e-7;
 			double tl = t - stepper.current_time_step();
 			double tr = t;
-			double tmid = tl + (tr - tl)/2.;
+			double tmid = 0;
 			while (prec_x>prec){
 				stepper.calc_state(tl, xbefore);
 				stepper.calc_state(tr, xafter);
@@ -92,72 +92,8 @@ int main() {
 			break;
 		}
 
-		outfile << t << " " << x[0] << " " << x[1] << std::endl;	
-		// std::cout << tafter << " " << xafter[0] << std::endl;
+		outfile << t << " " << x[0] << " " << x[1] << std::endl;
 		}
 
     return 0;
 }
-
-
-
-// #include <iostream>
-// #include <random>
-// #include <vector>
-// #include <fstream>
-// #include <boost/numeric/odeint.hpp>
-
-
-// std::random_device seed;
-// std::mt19937 engine(seed());
-// std::normal_distribution<> dist(0., 1.);
-
-// using namespace std;
-// using namespace boost::numeric::odeint;
-
-// typedef vector<double> state_type;
-
-// void harmonic_oscillator(const state_type &x, state_type &dxdt, double t) {
-//     const double omega = 1.0;
-//     dxdt[0] = x[1];
-//     dxdt[1] = -omega * omega * x[0];
-// }
-
-// void output_to_file(state_type &x, double t, ofstream &outfile) {
-//   outfile << t << " " << x[0] << " " << x[1] << endl;
-// }
-
-// void AddNoise(state_type &x, double t) {
-//   if(t<5) x[0] += 0.1*dist(engine);
-// }
-
-
-// int main() {
-//     state_type x(2);
-//     x[0] = 1.0;
-//     x[1] = 0.0;
-
-//     double ti = 0.0;
-//     double t_end = 10.0;
-//     double dt = 0.2;
-
-//     // stepper
-// 		// runge_kutta4<state_type> stepper;
-// 		typedef runge_kutta_dopri5<state_type>base_stepper_type;
-// 		auto stepper = make_dense_output( 1.0e-10 , 1.0e-6 , base_stepper_type());
-
-
-//     // 出力ファイルのオープン
-//     ofstream outfile("harmonic_oscillator.dat");
-
-// 		auto observer = [&outfile](state_type &x, double t) {
-//       output_to_file(x, t, outfile);
-// 			// AddNoise(x,t);
-//     };
-
-//     integrate_const(stepper, harmonic_oscillator, x, ti, t_end, dt, observer);
-
-//     outfile.close();
-
-//     return 0;
-// }
